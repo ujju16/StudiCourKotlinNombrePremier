c6c6839873e06804e2bd41cd411fb7dd7fedab38
Julien PERRIN <jul.darrouza@gmail.com>
2022-08-27 23:13:01 +0000

    Mise en place de demande d'un nombre

--- main.kt
+++ main.kt
@@ -1,5 +1,8 @@
 import java.util.*
 
+
 fun main() {
- 
+ val input.Scanner(System.`in`) // Demande action utilisateur
+ println("Choisir un nombre") // Demande d'un nombre à l'utilisateur
+ var nbre = input.nextInt()
 }
