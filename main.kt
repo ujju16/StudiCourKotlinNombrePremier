801cbd20df63f1e35ef3df11f358009a39dbda53
Julien PERRIN <jul.darrouza@gmail.com>
2022-08-27 23:17:44 +0000

    Construction de la condition

--- main.kt
+++ main.kt
@@ -5,4 +5,15 @@ fun main() {
  val input.Scanner(System.`in`) // Demande action utilisateur
  println("Choisir un nombre") // Demande d'un nombre à l'utilisateur
  var nbre = input.nextInt()
+ var flag = false
+ for (i in 2.. nbre / 2) {
+   if (nbre % i == 0) {
+     flag = true
+     break
+   }
+ }
+if (!flag)
+  println("$nbre est un nombre premier")
+else
+  println("$nbre n'est pas un nombre premier")
 }
